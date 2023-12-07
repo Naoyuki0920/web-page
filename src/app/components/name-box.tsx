22bafcbf8aab3be1d857ee56d656de75a23a7cac
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-12-07 16:43:56 +0900

    finish all responsive

--- src/app/components/name-box.tsx
+++ src/app/components/name-box.tsx
@@ -1,23 +1,43 @@
 "use client";
 
-import { Text, Flex, Stack, Spacer, Image } from "@chakra-ui/react";
+import { Text, Flex, Stack, Spacer, Image, Box, Center } from "@chakra-ui/react";
 
 export default function NameBox() {
   return (
-    <Flex flex={1} justify="space-between" maxW="5xl" mx="auto" padding={5}>
-      <Stack direction={["row", "column"]} align={["center", "stretch"]}>
-        <Text as="b" fontSize="4xl">
+    <>
+      <Box display={{ base: "none", sm: "block" }}>
+        <Flex flex={1} justify="space-between" maxW="5xl" mx="auto" padding={5}>
+          <Stack direction={["row", "column"]} align={["center", "stretch"]}>
+            <Text as="b" fontSize="4xl">
+              Naoyuki Masuda
+            </Text>
+            <Text>Android App Creator & Network Engineer</Text>
+          </Stack>
+          <Spacer />
+          <Image
+            borderRadius="full"
+            boxSize="100px"
+            src="./image/profile.jpg"
+            alt="img"
+          ></Image>
+        </Flex>
+      </Box>
+      <Box display={{ base: "block", sm: "none" }}>
+        
+        <Center><Text as="b" fontSize="4xl" width="100%" textAlign="center">
           Naoyuki Masuda
-        </Text>
-        <Text>Android App Creator & Network Engineer</Text>
-      </Stack>
-      <Spacer />
-      <Image
-        borderRadius="full"
-        boxSize="100px"
-        src="./image/profile.jpg"
-        alt="img"
-      ></Image>
-    </Flex>
+        </Text></Center>
+        <Stack direction="row" align={["center", "stretch"]}>
+          <Text textAlign="right">Android App Creator & Network Engineer</Text>
+          <Spacer />
+          <Image
+            borderRadius="full"
+            boxSize="100px"
+            src="./image/profile.jpg"
+            alt="img"
+          ></Image>
+        </Stack>
+      </Box>
+    </>
   );
 }
--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -5,7 +5,7 @@ import { LinkIcon } from "@chakra-ui/icons";
 
 export default function Title({ children }: { children: React.ReactNode }) {
   return (
-    <Stack direction={["column", "row"]}>
+    <Stack direction="row">
       <LinkIcon height="30px"></LinkIcon>
       <Text as={"b"} fontSize="xl">
         {children}
